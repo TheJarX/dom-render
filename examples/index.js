4431315fa982f287317cc8a62c6157486ad323b4
Gerard Altamirano <galtamiranor@outlook.es>
2020-06-12 16:51:40 -0500

    This branch is to add features a fix bugs. In this commit I added: a new dev branch, and a new way to use renderElement() using a string instead of the result of createHTML()

--- examples/index.js
+++ examples/index.js
@@ -8,8 +8,9 @@ window.addEventListener('load', e => {
     Domrender = new DOMRender();
     // TODO: test this
     Domrender.renderElement("#root", Domrender.createHTML("<h1>Hello, DOM! A2</h1>"));
+    Domrender.renderElement("#root", "<h1>Hello, DOM! A3</h1>");
 
-    // Another way
+    // Another way of render just one element
     Domrender = new DOMRender('#root');
     Domrender.setElement("<h1>Hello, DOM! B1</h1>");
     Domrender.renderElement();
